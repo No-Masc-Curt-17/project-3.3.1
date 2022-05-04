407693e72582f698fd99863ca812345aa038cc09
No-Masc-Curt-17 <91641926+No-Masc-Curt-17@users.noreply.github.com>
2022-05-04 13:55:28 -0700

    Add files via upload

--- project 3.3.1.py
+++ project 3.3.1.py
@@ -1,11 +1,21 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-europe = ["United States", "England", "Russia", "Germany", "Japan", "Saudi Arabia", "India", "China"]
+big_countries = ["United States", "England", "Russia", "Germany", "Japan", "Saudi Arabia", "India", "China"]
 df = pd.read_csv("population-by-country.csv", header=0)
 unique_countries = df['Entity'].unique()
+#average
+total = 0
+country_count = 0
+for i in range(len(df(['Entity']))):
+  if df['Entity'][i] in big_countries:
+    total += df['Total_Population'][i]
+    country_count +=1
+average = total/country_count
+print("average:", average)
+
 for c in unique_countries:
-  if c in europe:
+  if c in big_countries:
     years = df[df['Entity'] == c]['Year']
     total_population =(df[df['Entity'] == c]['Total_population'])
 
